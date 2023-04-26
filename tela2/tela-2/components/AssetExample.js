d543ef3f4a69398ac04007ccc5cb2573947d5a76
GabrielCecconi9 <99411346+GabrielCecconi9@users.noreply.github.com>
2023-04-26 17:36:53 -0300

    Add files via upload

---
+++ tela2/tela-2/components/AssetExample.js
@@ -0,0 +1,72 @@
+import * as React from 'react';
+import { Text, View, StyleSheet, Image, Button,SafeAreaView,TextInput } from 'react-native';
+
+export default function AssetExample() {
+  return (
+    <View style={styles.container}>
+     
+      <Image style={styles.logo} source={require('../assets/blaze.png')} />
+       <Text style={styles.paragraph}>
+         <SafeAreaView>
+      <TextInput
+        style={styles.input}
+        value={Number}
+        placeholder="Email"
+        keyboardType="numeric"
+      />
+      <TextInput
+        style={styles.input}
+        value={Number}
+        placeholder="senha"
+        keyboardType="numeric"
+      />
+    </SafeAreaView>
+         </Text>
+      <Text> 
+      <Button
+     title="Entrar"
+    color=""
+    
+  />
+
+      </Text>
+    </View>
+    
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 24,
+    backgroundColor: 'black',
+    
+  
+  },
+  
+  logo: {
+    height: 115,
+    width: 300,
+    marginBottom:200
+    
+  
+  },
+   paragraph: {
+    margin: 1,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: 'white',
+   },
+  
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    backgroundColor: "white"
+    
+  }
+
+});
---
+++ tela4/tela 4/components/AssetExample.js
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import { Text, View, StyleSheet, Image, Button } from 'react-native';
+
+export default function AssetExample() {
+  return (
+    <View style={styles.container}>
+     
+      <Image style={styles.logo} source={require('../assets/telaaposta.png')} />
+       <Text style={styles.paragraph}>
+        
+         </Text>
+      
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 24,
+    backgroundColor: 'black',
+    
+  
+  },
+  
+  logo: {
+    height: 770,
+    width: 400,
+    
+    
+  
+  },
+   paragraph: {
+    margin: 1,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: 'white',
+   }
+});
